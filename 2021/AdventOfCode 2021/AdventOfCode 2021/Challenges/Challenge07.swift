6dd422f492a72a1f64995e60f788c941ecf1414f
Chris Downie <cdownie@gmail.com>
2021-12-07 06:34:30 -0800

    [2021.07] Solve part 1 with demo input

--- 2021/AdventOfCode 2021/AdventOfCode 2021/Challenges/Challenge07.swift
+++ 2021/AdventOfCode 2021/AdventOfCode 2021/Challenges/Challenge07.swift
@@ -7,11 +7,46 @@
 
 import SwiftUI
 
+private let demoInput = "16,1,2,0,4,2,7,1,2,14"
+
 class CrabSubFormations: ObservableObject {
     @Published var output = ""
+    private let crabs: [Int]
+    private let isDemo = true
+
+    init() {
+        let input: String
+        if !isDemo {
+            let inputData = NSDataAsset(name: "07")!.data
+            input = String(data: inputData, encoding: .utf8)!
+        } else {
+            input = demoInput
+        }
+        
+        crabs = input
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .components(separatedBy: ",")
+            .compactMap(Int.init)
+    }
     
     func solve() {
-        output = "test"
+        let maxPosition = crabs.reduce(0, max)
+        let bestPair = (0...maxPosition)
+            .map { alignOnPosition -> (position: Int, cost: Int) in
+                let cost = crabs
+                    .map { abs(alignOnPosition - $0) }
+                    .reduce(0, +)
+                return (position: alignOnPosition, cost: cost)
+            }
+            .reduce((position: -1, cost: Int.max)) { bestPair, thisPair in
+                if thisPair.cost < bestPair.cost {
+                    return thisPair
+                } else {
+                    return bestPair
+                }
+            }
+        
+        output = "\(bestPair.cost)"
     }
 }
 
