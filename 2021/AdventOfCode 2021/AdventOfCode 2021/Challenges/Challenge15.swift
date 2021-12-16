075ec82ef02ba8832006f33dc5e883a5b77141fa
Chris Downie <cdownie@gmail.com>
2021-12-15 17:09:24 -0800

    [2021.15] Boilerplate for Challenge 15

---
+++ 2021/AdventOfCode 2021/AdventOfCode 2021/Challenges/Challenge15.swift
@@ -0,0 +1,70 @@
+//
+//  Challenge15.swift
+//  AdventOfCode 2021
+//
+//  Created by Chris Downie on 12/15/21.
+//
+
+import SwiftUI
+
+private let demoInput = """
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+
+"""
+
+class PathFinder: ObservableObject {
+    @Published var output = ""
+    private let isDemo = true
+    private let isPartTwo = false
+    
+    init() {
+        let input: String
+        if isDemo {
+            input = demoInput
+        } else {
+            let inputData = NSDataAsset(name: "15")!.data
+            input = String(data: inputData, encoding: .utf8)!
+        }
+        print(input)
+    }
+    
+    func solve() {
+        output = "Fake"
+    }
+}
+
+struct Challenge15: View {
+    @StateObject var state = PathFinder()
+    let pasteboard = NSPasteboard.general
+    
+    var body: some View {
+        VStack {
+            Text(state.output)
+                .font(.system(.body, design: .monospaced))
+                .onAppear {
+                    state.solve()
+                }
+            Button {
+                pasteboard.prepareForNewContents()
+                _ = pasteboard.setString(state.output, forType: .string)
+            } label: {
+                Label("Copy", systemImage: "doc.on.doc")
+            }
+        }
+        .frame(minWidth: 200)
+    }
+}
+struct Challenge15_Previews: PreviewProvider {
+    static var previews: some View {
+        Challenge15()
+    }
+}
--- 2021/AdventOfCode 2021/AdventOfCode 2021/ContentView.swift
+++ 2021/AdventOfCode 2021/AdventOfCode 2021/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Challenge14()
+        Challenge15()
             .padding()
     }
 }
