f4c78055d30e024be46eda5fedfac9d3e26ca5f0
Chris Downie <cdownie@gmail.com>
2021-12-18 10:45:19 -0800

    [2021.18] Add tests for addition with complex reduction

--- 2021/AdventOfCode 2021/AdventOfCode 2021Tests/SnailfishParsingTests.swift
+++ 2021/AdventOfCode 2021/AdventOfCode 2021Tests/SnailfishParsingTests.swift
@@ -81,6 +81,13 @@ class SnailfishParsingTests: XCTestCase {
 
 // MARK: - Addition
 class SnailfishNumberAdditionTests: XCTestCase {
+    private let ones   = SnailfishNumber.pair(left: .value(1), right: .value(1))
+    private let twos   = SnailfishNumber.pair(left: .value(2), right: .value(2))
+    private let threes = SnailfishNumber.pair(left: .value(3), right: .value(3))
+    private let fours  = SnailfishNumber.pair(left: .value(4), right: .value(4))
+    private let fives  = SnailfishNumber.pair(left: .value(5), right: .value(5))
+    private let sixes  = SnailfishNumber.pair(left: .value(6), right: .value(6))
+    
     func testAdditionWithoutAnyReduction() {
         let left = SnailfishNumber.pair(left: .value(1), right: .value(2))
         let right = SnailfishNumber.pair(left: .pair(left: .value(3), right: .value(4)), right: .value(5))
@@ -89,6 +96,24 @@ class SnailfishNumberAdditionTests: XCTestCase {
             left: .pair(left: .value(1), right: .value(2)),
             right: .pair(left: .pair(left: .value(3), right: .value(4)), right: .value(5))
         )
+        XCTAssertEqual(result, expected)
+    }
+    
+    func testMultipleAdditionWithoutAnyReduction() {
+        let result = ones + twos + threes + fours
+        let expected = SnailfishNumber.from(string: "[[[[1,1],[2,2]],[3,3]],[4,4]]")
+        XCTAssertEqual(result, expected)
+    }
+    
+    func testMultipleAdditionWithSingleReduction() {
+        let result = ones + twos + threes + fours + fives
+        let expected = SnailfishNumber.from(string: "[[[[3,0],[5,3]],[4,4]],[5,5]]")
+        XCTAssertEqual(result, expected)
+    }
+    
+    func testMultipleAdditionWithMultipleReduction() {
+        let result = ones + twos + threes + fours + fives + sixes
+        let expected = SnailfishNumber.from(string: "[[[[5,0],[7,4]],[5,5]],[6,6]]")
         XCTAssertEqual(result, expected)
     }
 }
