fdb4c5b232bc85c6ea6e1daa5d8f8b2d27730d4b
Chris Downie <cdownie@gmail.com>
2021-12-26 10:21:06 -0800

    Add callback-based interface to Solver

--- 2021/AdventOfCode 2021/AdventOfCode 2021/Solver.swift
+++ 2021/AdventOfCode 2021/AdventOfCode 2021/Solver.swift
@@ -8,29 +8,41 @@
 import Foundation
 import os
 
+private let solverQueue = OperationQueue()
+
 public class Solver {
+    private let backgroundQueue = solverQueue
     private let logger: Logger
     private let isPartTwo: Bool
     private var task: Task<String, Error>?
     private let input: String
-
+    
     required public init(input: String, isPartTwo: Bool, logger: Logger = Logger()) {
         self.input = input
         self.isPartTwo = isPartTwo
         self.logger = logger
     }
     
-    public func solve() async throws -> String {
-        let task = Task(priority: .userInitiated, operation: {
-            try solveOrThrow()
-        })
-        self.task = task
-        return try await task.value
-    }
-    
-    public func cancel() {
-        task?.cancel()
-        task = nil
+    /// Runs the solver on a background thread. The completion handler is called when a result is reached.
+    /// - Parameter completion: Called when this solver has completed its work.
+    public final func solve(completion: @escaping (Result<String, Error>) -> Void) {
+        let operation = BlockOperation { [weak self] in
+            guard let self = self else {
+                return
+            }
+            
+            do {
+                let result = try self.solveOrThrow()
+                OperationQueue.main.addOperation {
+                    completion(.success(result))
+                }
+            } catch {
+                OperationQueue.main.addOperation {
+                    completion(.failure(error))
+                }
+            }
+        }
+        backgroundQueue.addOperation(operation)
     }
     
     // Override these
@@ -39,5 +51,21 @@ public class Solver {
         Part \(isPartTwo ? 2 : 1): Not Implemented
         Input: \(input)
         """
+    }
+}
+
+// Async/Await interface
+extension Solver {
+    public func solve() async throws -> String {
+        let task = Task(priority: .userInitiated, operation: {
+            try solveOrThrow()
+        })
+        self.task = task
+        return try await task.value
+    }
+//    Dropping cancellation support for now.
+    public func cancel() {
+        task?.cancel()
+        task = nil
     }
 }
